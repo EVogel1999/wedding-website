de1806d8e838b6995b44278043998665a73eddf5
Emily Vogel <=>
2023-02-05 17:05:14 -0500

    Create initial page

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <RouterView />
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Alex+Brush&display=swap');
+
+body {
+  margin: 0;
+  padding: 0;
+
+  font-family: 'Alex Brush', cursive;
+  color: #1B1212;
+}
+</style>
